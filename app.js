81499d8af56927f07938bc3042dced1380636031
Silas Leite <bigchill_silas@hotmail.com>
2024-03-27 11:10:09 -0300

    :sparkles: feat: Routes to render create, read, update and delete pages; and realize CRUD operations in database

--- app.js
+++ app.js
@@ -1,22 +1,86 @@
 const path = require('node:path'); 
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const agendamento = require("./models/agendamento")
 const app = express();
 
 const handlebars = require("express-handlebars").engine;
-app.engine("handlebars", handlebars({defaultLayout: "main"}));
+app.engine("handlebars", handlebars({
+  defaultLayout: "main",
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true,
+  }
+}));
 app.set("view engine", "handlebars");
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get("/", function(req, res){    
   res.render("home")
 });
 
-app.get("/consultar", function(req, res){    
-  res.render("list")
+app.post('/cadastrar', function(req, res) {
+  agendamento.create({
+    nome: req.body.nome,
+    telefone: req.body.telefone,
+    origem: req.body.origem,
+    data_contato: req.body.data_contato,
+    observacao: req.body.observacao
+  }).then(function(){
+      console.log("Agendamento cadastrado com sucesso!")
+  }).catch(function(erro){
+      console.log("Erro: Agendamento não cadastrado!" + erro)
+  });
+
+  res.render('home');
 });
 
-app.get("/atualizar", function(req, res){    
-  res.render("update")
+app.get("/consultar", function(req, res){    
+  agendamento.findAll().then(result => {
+    res.render('list', { result });
+  }).catch((erro) => {
+    return res.status(401).json({ error: 'Houve um error'});
+  });
+});
+
+app.get("/atualizar/:id", function(req, res){    
+  agendamento.findOne({ where: { id: req.params.id }}).then((result) => {
+    res.render('update', { result });
+  }).catch((err) => {
+    return res.status(401).json({ error: 'Houve um erro' });
+  });
+});
+
+app.post("/atualizar", function(req, res){    
+  agendamento.update({
+    nome: req.body.nome,
+    telefone: req.body.telefone,
+    origem: req.body.origem,
+    data_contato: req.body.data_contato,
+    observacao: req.body.observacao
+  }, {
+    where: {
+      id: req.body.id
+    }
+  }).then(() => {
+    res.redirect('/consultar');
+  }).catch(function (erro) {
+    return res.status(401).json({ error: 'Houve um erro' });
+  });
+});
+
+app.get("/deletar/:id", function(req, res){    
+  agendamento.destroy({
+    where: { id: req.params.id }
+  }).then(function () {
+    res.redirect('/consultar');
+  }).catch(function (erro) {
+    return res.status(401).json({ error: 'Houve um erro' });
+  });
 });
 
 app.listen(8081, function(){    
